b16a6eaf11f29a5bbadddc32b43d35e2c10aa187
priyanknamdeo16 <namdeo.priyank@gmail.com>
2017-10-22 18:04:26 +0530

    separate files

--- myproject/src/app/app.module.ts
+++ myproject/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeroDetailComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ myproject/src/app/hero.ts
@@ -0,0 +1,5 @@
+//hero class defined
+export class Hero {
+  public id:number;
+  public name:string;
+}
