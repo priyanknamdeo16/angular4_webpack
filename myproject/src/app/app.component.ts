eab1b4dc159edbc258219c22ec51f7a07084602e
priyanknamdeo16 <namdeo.priyank@gmail.com>
2017-10-22 17:33:58 +0530

    components class

--- myproject/src/app/app.component.ts
+++ myproject/src/app/app.component.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
 
+//hero class defined
+export class Hero {
+  public id:number;
+  public name:string;
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Neha' },
+  { id: 12, name: 'Iti' },
+  { id: 13, name: 'Priyank' }
+];
+
+//decorator for AppComponent
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,18 +20,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title : string;
-  public hero : Hero;
+  public selectedHero : Hero;
+  public heroes: Hero[];
 
+  //constructor
   constructor () {
     this.title = 'tour of heroes';
-    this.hero  = {
-      id : 1,
-      name : 'priyank'
-    };
+    //this.hero  = HEROES[0];
+    this.heroes = HEROES;
+  }
+
+  //on select listner
+  onSelect = (hero) => {
+    this.selectedHero = hero;
   }
 }
 
-export class Hero {
-  public id:number;
-  public name:string;
-}
+
