92ecea5bbc75f4b2e881af6cd75ac2d00254c8a0
priyanknamdeo16 <namdeo.priyank@gmail.com>
2017-10-22 15:25:50 +0530

    Tour of hero implementation started

--- myproject/src/app/app.component.ts
+++ myproject/src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'priyank apps';
+  public title : string;
+  public hero : Hero;
+
+  constructor () {
+    this.title = 'tour of heroes';
+    this.hero  = {
+      id : 1,
+      name : 'priyank'
+    };
+  }
+}
+
+export class Hero {
+  public id:number;
+  public name:string;
 }
